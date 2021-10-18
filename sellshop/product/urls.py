d6f6e50bc01364255f260c09075709ecbc622b90
Vusal <vusal581176@gmail.com>
2021-10-18 11:18:18 +0400

    Before Merge

--- sellshop/product/urls.py
+++ sellshop/product/urls.py
@@ -4,7 +4,7 @@ from . import views
 # app_name = 'product'
 
 urlpatterns = [
-    path('single-product/<int:pk>/', views.single_product, name="single_product"),
+    path('single-product/<int:pk>/', views.ProductDetailView.as_view(), name="single_product"),
     path('product-list/', views.ProductListView.as_view(), name="product_list"),
 ]
 
--- sellshop/product/views.py
+++ sellshop/product/views.py
@@ -42,7 +42,10 @@ class ProductDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Single-product Sellshop'
+        context['images'] = Image.objects.filter(productversion=self.kwargs.get('pk'))
         context['form'] = ReviewForm
+        context['productversion'] = ProductVersion.objects.get(pk=self.kwargs.get('pk'))
+        context['relatedproducts'] = ProductVersion.objects.exclude(pk=self.kwargs.get('pk'))
         context['reviews'] = Review.objects.filter(product=self.kwargs.get('pk'))
         return context
 
