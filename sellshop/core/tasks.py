00c54037c9c63e31792411eeccc09304ac1f9ac7
Vusal <vusal581176@gmail.com>
2021-10-29 13:46:46 +0400

    Email

--- sellshop/core/tasks.py
+++ sellshop/core/tasks.py
@@ -6,14 +6,16 @@ from django.template.loader import render_to_string
 from django.core.mail import EmailMessage
 from django.conf import settings
 from product.models import ProductVersion
+from django.utils import timezone
+from datetime import timedelta
 
 
 @shared_task
 def send_email_to_subscribers():
+    present_time = timezone.now()
+    end_date = present_time - timedelta(days=7)
     subscribers_emails = Subscriber.objects.values_list('email', flat=True)
-    # products = ProductVersion.objects.all()
-    products = ProductVersion.objects.annotate(num_tags=Count('reviews')).order_by('-num_tags')[0:3]
-    # products = ProductVersion.objects.filter('-created_at').annotate(num_tags=Count('reviews')).order_by('-num_tags')[0:3]
+    products = ProductVersion.objects.filter(created_at__gte=end_date).annotate(num_tags=Count('reviews')).order_by('-num_tags')[0:3]
     for mail in subscribers_emails:
         body = render_to_string("subscribers_email.html", context= {
             'email': mail,
