881100ea6727acfc5e0a32edd956c1b8132c8186
Vusal <vusal581176@gmail.com>
2021-11-10 21:26:28 +0400

    Date fixed

--- sellshop/sellshop/utils/base_models.py
+++ sellshop/sellshop/utils/base_models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # from django.utils import timezone
 class BaseModel(models.Model):
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
     class Meta:
         abstract = True
